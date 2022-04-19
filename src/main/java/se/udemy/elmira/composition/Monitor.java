6cfa9912d8a24fbe6060647ea3be5d1ff280597b
EELMMAD <elmiramadadi@gmail.com>
2022-04-19 20:54:18 +0200

    Add some more features at Monitor.java.

--- src/main/java/se/udemy/elmira/composition/Monitor.java
+++ src/main/java/se/udemy/elmira/composition/Monitor.java
@@ -1,5 +1,35 @@
 package se.udemy.elmira.composition;
 
 public class Monitor {
+   private String model;
+   private String manufacture;
+   private int size;
+   private Resolution nativeResolution;   //Composition  ==> Monitor HAS_A Resolution
 
+    public Monitor(String model, String manufacture, int size, Resolution nativeResolution) {
+        this.model = model;
+        this.manufacture = manufacture;
+        this.size = size;
+        this.nativeResolution = nativeResolution;
+    }
+
+    public void drawPixelAt(int x, int y, String color){
+        System.out.println("Drawing pixel at " + x + "," + y + " in colour " + color);
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getManufacture() {
+        return manufacture;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Resolution getNativeResolution() {
+        return nativeResolution;
+    }
 }
