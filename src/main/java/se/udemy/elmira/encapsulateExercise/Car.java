565b56818378251e1f5334c6f7d4999e21ac3e64
EELMMAD <elmiramadadi@gmail.com>
2022-04-23 23:23:49 +0200

    Create Printer.java

---
+++ src/main/java/se/udemy/elmira/encapsulateExercise/Car.java
@@ -0,0 +1,39 @@
+package se.udemy.elmira.encapsulateExercise;
+
+public class Car {
+
+        private boolean engine;
+        private int cylinders;
+        private String name;
+        private int wheels;
+
+        public int getCylinders() {
+            return cylinders;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Car(int cylinders, String name) {
+            this.cylinders = cylinders;
+            this.name = name;
+            this.engine = true;
+            this.wheels = 4;
+
+
+        }
+
+        public String startEngine() {
+            return "The " + getClass().getSimpleName() + " engine is starting.";
+        }
+
+        public String accelerate() {
+            return "The " + getClass().getSimpleName() + " is accelerating.";
+        }
+
+        public String brake() {
+            return "The "+getClass().getSimpleName() +" is braking.";
+        }
+    }
+
