ba778ff4c1b4a0bf6b0a0a6c3c8a31ef022eb8bf
EELMMAD <elmiramadadi@gmail.com>
2022-04-24 22:23:24 +0200

    Add randomMovie method to Main.java

--- src/main/java/se/udemy/elmira/polymorphism/Main.java
+++ src/main/java/se/udemy/elmira/polymorphism/Main.java
@@ -63,10 +63,30 @@ class Forgetable extends Movie {
 }
 
 
-
-
 public class Main {
     public static void main(String[] args) {
 
+    }
+
+    public static Movie randomMovie() {
+        int randomNumber = (int) (Math.random() * 5) + 1;
+        System.out.println("Random number generated was: " + randomNumber);
+        switch (randomNumber){
+            case 1:
+                return new Jaws();
+
+            case 2:
+                return new IndependenceDay();
+
+            case 3:
+                return new MazeRunner();
+
+            case 4:
+                return new StarWars();
+
+            case 5:
+                return new Forgetable();
+        }
+        return null;
     }
 }
